b65ed1353c2c3179fe79683c63bb84e022916c4e
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-03-03 23:30:54 +0500

    Pages Down

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,8 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={"lol"} />
-      <Route path="/contact" element={"Noman Ali"} />
+      <Route path="/" element={"home"} />
+      <Route path="/chats" element={"home"} />
     </Routes>
   );
 }
---
+++ frontend/src/components/ChatPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const chatPage = () => {
+  return <div>ChatPage</div>;
+};
+
+export default chatPage;
