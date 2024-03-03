bfb52c21556cc3873a936c73be4c12ac995a6985
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-03-04 00:00:40 +0500

    Clean the Index File

--- frontend/src/components/ChatPage.js
+++ frontend/src/components/ChatPage.js
@@ -1,16 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const ChatPage = () => {
+  const [chats, setChats] = useState([]);
+
   const fetchChats = async () => {
-    const data = await axios.get("http://localhost:5000/api/chats");
-    console.log(data);
+    const { data } = await axios.get("http://localhost:5000/api/chats");
+    setChats(data);
   };
 
   useEffect(() => {
     fetchChats();
   }, []);
-  return <div>ChatPage</div>;
+  return (
+    <div>
+      ChatPage :{" "}
+      {chats?.map((chat, index) => (
+        <h2 key={index}>{chat.chatName}</h2>
+      ))}
+    </div>
+  );
 };
 
 export default ChatPage;
