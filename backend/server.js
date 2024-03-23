de09ca305b34cbd46e77c29b87d3f09d3a591245
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-03-23 18:07:52 +0500

    User Registeration Done

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const { chats } = require("./data/data");
 const app = express();
 const cors = require("cors");
 const connectDB = require("./config/db");
-
+const userRoutes = require("./routes/userRoutes");
 dotenv.config();
 connectDB();
 app.use(cors());
+app.use(express.json());
+
+app.use('/api/user', userRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is Running");
 });
 
-app.get("/api/chats", (req, res) => {
-  res.send(chats);
-});
 
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((chat) => chat._id === req.params.id);
-  res.send(singleChat);
-});
+
 
 const PORT = process.env.PORT | 5000;
 app.listen(PORT, () => {
