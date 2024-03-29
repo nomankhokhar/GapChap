593df8729e00eb80f4a30157d543fd4c3754eaae
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-03-29 17:24:27 +0500

    Feat : Sidebar Functionality 70% Done

---
+++ frontend/src/components/ChatLoading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Stack } from '@chakra-ui/layout'
+
+const ChatLoading = () => {
+  return (
+    <Stack>
+      
+    </Stack>
+  )
+}
+
+export default ChatLoading
--- frontend/src/components/miscellanenious/SideDrawer.js
+++ frontend/src/components/miscellanenious/SideDrawer.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Button } from "@chakra-ui/button";
-// import { useDisclosure } from "@chakra-ui/hooks";
-// import { Input } from "@chakra-ui/input";
+import { useDisclosure } from "@chakra-ui/hooks";
+import { Input } from "@chakra-ui/input";
 import { Box, Text } from "@chakra-ui/layout";
 import {
   Menu,
@@ -10,29 +10,26 @@ import {
   MenuItem,
   MenuList,
 } from "@chakra-ui/menu";
-// import {
-//   Drawer,
-//   DrawerBody,
-//   DrawerContent,
-//   DrawerHeader,
-//   DrawerOverlay,
-// } from "@chakra-ui/modal";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+} from "@chakra-ui/modal";
+import { useToast } from "@chakra-ui/toast";
 import { Tooltip } from "@chakra-ui/tooltip";
 import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import { Avatar } from "@chakra-ui/avatar";
 import { ChatState } from '../../Context/ChatProvider';
 import ProfileModal from './ProfileModal';
-// import { useHistory } from "react-router-dom";
-// import axios from "axios";
-// import { useToast } from "@chakra-ui/toast";
+import axios from "axios";
 // import ChatLoading from "../ChatLoading";
-// import { Spinner } from "@chakra-ui/spinner";
-// import ProfileModal from "./ProfileModal";
+import { Spinner } from "@chakra-ui/spinner";
 // import NotificationBadge from "react-notification-badge";
 // import { Effect } from "react-notification-badge";
 // import { getSender } from "../../config/ChatLogics";
 // import UserListItem from "../userAvatar/UserListItem";
-// import { ChatState } from "../../Context/ChatProvider";
 import { useNavigate } from 'react-router-dom';
 
 
@@ -43,13 +40,55 @@ const SideDrawer = () => {
   const [loadingChat, setLoadingChat] = useState(false);
 
   const navigate = useNavigate();
-
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { user }= ChatState();
 
   const logoutHandler = () => {
     localStorage.removeItem("userInfo");
     navigate("/")
   }
+  
+  const toast = useToast();
+
+  const handleSearch = async () => {
+    if (!search) {
+      toast({
+        title: "Please fill the search...",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-right",
+      });
+    };
+
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      const { data } = await axios.get(
+        `http://localhost:5000/api/user?search=${search}`,
+        config
+      );
+
+      setSearchResult(data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      toast({
+        title: "Error Occured!",
+        description: 'Failed to load the search result',
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "top-right",
+      });
+    }
+  };
 
   return (
     <>
@@ -67,7 +106,7 @@ const SideDrawer = () => {
         borderWidth="5px"
       >
      <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
-          <Button variant="ghost">
+          <Button variant="ghost" onClick={onOpen}>
             <i className="fas fa-search"></i>
             <Text d={{ base: "none", md: "flex" }} px={4}>
               Search User
@@ -98,6 +137,39 @@ const SideDrawer = () => {
         </Menu>
        </div>
       </Box>
+      <Drawer placement='left' isOpen={isOpen} onClose={onClose}>
+      <DrawerOverlay />
+      <DrawerContent >
+        <DrawerHeader borderBottomWidth='1px'>
+          Search Users
+        </DrawerHeader>
+        <DrawerBody>
+          <Box style={{display:"flex"}} pb={2}>
+            <Input 
+            placeholder='Search Users by name or email'
+            mr={2}
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            />
+            <Button 
+            onClick={handleSearch}
+            >Go</Button>
+          </Box>
+          {/*Button onClick={() => setLoading(true)}>Search</Button>ton onClick={() => setLoading(true)}>Search</Button>loading ? (
+            <ChatLoading />
+          ) : (
+            searchResult.map((user) => (
+              <UserListItem
+                key={user._id}
+                user={user}
+                onClose={onClose}
+                setLoadingChat={setLoadingChat}
+              />
+            ))
+          )} */}
+        </DrawerBody>
+      </DrawerContent>
+      </Drawer>
     </>
   )
 }
