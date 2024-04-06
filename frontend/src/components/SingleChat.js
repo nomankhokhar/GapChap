02ca305392bcd5419d0037c233de57ca274f3634
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-04-06 22:09:54 +0500

    feat : Add Group Chat Model and Functionality

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -1,5 +1,3 @@
-import React from 'react'
-import { ChatState } from '../Context/ChatProvider';
 import { FormControl } from "@chakra-ui/form-control";
 import { Input } from "@chakra-ui/input";
 import { Box, Text } from "@chakra-ui/layout";
@@ -9,19 +7,157 @@ import { getSender, getSenderFull } from "../config/ChatLogics";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { ArrowBackIcon } from "@chakra-ui/icons";
-import ProfileModal from "./miscellaneous/ProfileModal";
-import ScrollableChat from "./ScrollableChat";
-import Lottie from "react-lottie";
-import animationData from "../animations/typing.json";
-import io from "socket.io-client";
-import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
-const ENDPOINT = "http://localhost:5000";
-var socket, selectedChatCompare;
+// import ScrollableChat from "./ScrollableChat";
+// import Lottie from "react-lottie";
+import { ChatState } from "../Context/ChatProvider";
+import UpdateGroupChatModal from "./miscellanenious/UpdateGroupChatModal";
+import ProfileModal from "./miscellanenious/ProfileModal";
+// import animationData from "../animations/typing.json";
 
+// import io from "socket.io-client";
+const ENDPOINT = "http://localhost:5000"; 
+var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain , setFetchAgain}) => {
   
-    const { user , selectedChat, setSelectedChat } = ChatState();
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [newMessage, setNewMessage] = useState("");
+  const [socketConnected, setSocketConnected] = useState(false);
+  const [typing, setTyping] = useState(false);
+  const [istyping, setIsTyping] = useState(false);
+  const toast = useToast();
+
+  // const defaultOptions = {
+  //   loop: true,
+  //   autoplay: true,
+  //   animationData: animationData,
+  //   rendererSettings: {
+  //     preserveAspectRatio: "xMidYMid slice",
+  //   },
+  // };
+  const { selectedChat, setSelectedChat, user } =
+    ChatState();
+
+  const fetchMessages = async () => {
+    if (!selectedChat) return;
+
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      setLoading(true);
+
+      const { data } = await axios.get(
+        `http://localhost:5000/api/message/${selectedChat._id}`,
+        config
+      );
+      setMessages(data);
+      setLoading(false);
+
+      socket.emit("join chat", selectedChat._id);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to Load the Messages",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+    }
+  };
+
+  // const sendMessage = async (event) => {
+  //   if (event.key === "Enter" && newMessage) {
+  //     socket.emit("stop typing", selectedChat._id);
+  //     try {
+  //       const config = {
+  //         headers: {
+  //           "Content-type": "application/json",
+  //           Authorization: `Bearer ${user.token}`,
+  //         },
+  //       };
+  //       setNewMessage("");
+  //       const { data } = await axios.post(
+  //         "http://localhost:5000/api/message",
+  //         {
+  //           content: newMessage,
+  //           chatId: selectedChat,
+  //         },
+  //         config
+  //       );
+  //       socket.emit("new message", data);
+  //       setMessages([...messages, data]);
+  //     } catch (error) {
+  //       toast({
+  //         title: "Error Occured!",
+  //         description: "Failed to send the Message",
+  //         status: "error",
+  //         duration: 5000,
+  //         isClosable: true,
+  //         position: "bottom",
+  //       });
+  //     }
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   socket = io(ENDPOINT);
+  //   socket.emit("setup", user);
+  //   socket.on("connected", () => setSocketConnected(true));
+  //   socket.on("typing", () => setIsTyping(true));
+  //   socket.on("stop typing", () => setIsTyping(false));
+
+  //   // eslint-disable-next-line
+  // }, []);
+
+  // useEffect(() => {
+  //   fetchMessages();
+
+  //   selectedChatCompare = selectedChat;
+  //   // eslint-disable-next-line
+  // }, [selectedChat]);
+
+  // useEffect(() => {
+  //   socket.on("message recieved", (newMessageRecieved) => {
+  //     if (
+  //       !selectedChatCompare || // if chat is not selected or doesn't match current chat
+  //       selectedChatCompare._id !== newMessageRecieved.chat._id
+  //     ) {
+  //       if (!notification.includes(newMessageRecieved)) {
+  //         setNotification([newMessageRecieved, ...notification]);
+  //         setFetchAgain(!fetchAgain);
+  //       }
+  //     } else {
+  //       setMessages([...messages, newMessageRecieved]);
+  //     }
+  //   });
+  // });
+
+  // const typingHandler = (e) => {
+  //   setNewMessage(e.target.value);
+
+  //   if (!socketConnected) return;
+
+  //   if (!typing) {
+  //     setTyping(true);
+  //     socket.emit("typing", selectedChat._id);
+  //   }
+  //   let lastTypingTime = new Date().getTime();
+  //   var timerLength = 3000;
+  //   setTimeout(() => {
+  //     var timeNow = new Date().getTime();
+  //     var timeDiff = timeNow - lastTypingTime;
+  //     if (timeDiff >= timerLength && typing) {
+  //       socket.emit("stop typing", selectedChat._id);
+  //       setTyping(false);
+  //     }
+  //   }, timerLength);
+  // };
 
     return (
     <>
@@ -42,7 +178,7 @@ const SingleChat = ({ fetchAgain , setFetchAgain}) => {
               icon={<ArrowBackIcon />}
               onClick={() => setSelectedChat("")}
             />
-             {messages &&
+             {true &&
               (!selectedChat.isGroupChat ? (
                 <>
                   {getSender(user, selectedChat.users)}
@@ -59,8 +195,22 @@ const SingleChat = ({ fetchAgain , setFetchAgain}) => {
                     setFetchAgain={setFetchAgain}
                   />
                 </>
-              ))}
+              ))
+              }
           </Text>
+          <Box
+            display="flex"
+            flexDir="column"
+            justifyContent="flex-end"
+            p={3}
+            bg="#E8E8E8"
+            w="100%"
+            h="100%"
+            borderRadius="lg"
+            overflowY="hidden"
+          >
+
+          </Box>
       </>): 
       (<> 
       <Box display="flex" alignItems="center" justifyContent="center" h="100%">
